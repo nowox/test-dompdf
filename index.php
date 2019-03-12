1b0ef407f9ff1beba2846afa006c345c11e46e81
CHEVALLIER Yves <ycr@csem.ch>
2019-03-12 15:04:17 +0100

    Initial Commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Dompdf\Dompdf;
+use Dompdf\Options;
+
+$options = new Options();
+$options->set('defaultFont', 'Open Sans');
+$options->set('fontDir', 'vendor/webfontkit/open-sans');
+
+$dompdf = new Dompdf($options);
+$dompdf->loadHtml(file_get_contents('template.html'));
+
+
+$dompdf->setPaper('A4');
+$dompdf->render();
+$dompdf->stream();
